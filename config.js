8d4b69bea287a21c319298dbdb387c83f7d81e4d
Modybshh7 <148764426+Modybshh7@users.noreply.github.com>
2023-10-23 14:58:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919074692450', 'ABHISHEK-SURESH', true],
+  ['201553941991', 'مـــ[ـــــودي， ألـ بـــــأشـــــــــ]ـــأ $', true],
   [''],
   [''] 
 ] //Number of owners
 
-global.mods = ['919074692450','919074692450'] 
-global.prems = ['919074692450', '919074692450', '919074692450']
+global.mods = ['201553941991','201553941991'] 
+global.prems = ['201553941991', '201553941991', '919074692450']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,16 +47,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁'
+global.botname = 'مـــ[ـــــودي， ألـ بـــــأشــــــــــ]ـــأ $'
 global.premium = 'true'
-global.packname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️' 
-global.author = 'ABHISHEK SURESH' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/abhishek_ser\n' 
-global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
-global.fgsc = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' 
-global.fgyt = 'https://youtube.com/@comedymelodych8468'
-global.fgpyp = 'https://youtube.com/@comedymelodych8468'
-global.fglog = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png' 
+global.packname = 'مودي بيحبك😂♥️' 
+global.author = 'مـــ[ـــــودي， ألـ بـــــأشــــــــــ]ـــأ $' 
+global.igfg = '▢ Follow on Instagram\nhttps://instagram.com/mody_bshhh?igshid=NTc4MTIwNjQ2YQ==\n' 
+global.dygp = 'https://api.whatsapp.com/send?phone=+201013071220'
+global.fgsc = 'https://api.whatsapp.com/send?phone=+201013071220' 
+global.fgyt = 'https://instagram.com/mody_bshhh?igshid=NTc4MTIwNjQ2YQ=='
+global.fgpyp = 'https://instagram.com/mody_bshhh?igshid=NTc4MTIwNjQ2YQ=='
+global.fglog = 'https://instagram.com/mody_bshhh?igshid=NTc4MTIwNjQ2YQ==' 
 
 //imagen
 global.imagen1 = fs.readFileSync('./src/Abhi.jpg')
